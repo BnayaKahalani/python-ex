73e22f7ce4b11741a2fea8199b70db6e6f6273a5
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-15 17:16:09 +0200

    ex3 completed

---
+++ ex3.py
@@ -0,0 +1,16 @@
+# Strings in Python are immutable sequences of Unicode code points. Given a string, we need to find its length. Examples:
+
+# Input : 'abc'
+# Output : 3
+
+# Input : 'hello world !'
+# Output : 13
+
+# Input : ' h e l   l  o '
+# Output :14
+
+def find_len(str):
+  print(len(str))
+  
+find_len(" h e l   l  o ")
+
