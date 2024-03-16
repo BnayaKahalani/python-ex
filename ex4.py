d01b41cabeebb71e0aefce787aa84551a3df1a96
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-16 21:19:20 +0200

    add ex4

---
+++ ex4.py
@@ -0,0 +1,10 @@
+# Given a string. The task is to print all words with even length in the given string.
+
+# Examples: 
+
+# Input: s = "This is a python language"
+# Output: This is python language
+
+# Input: s = "i am laxmi"
+# Output: am
+
