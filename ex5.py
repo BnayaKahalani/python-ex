0881711a5d4ac415de4e2b9387a183cbcfbb264e
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-18 10:55:43 +0200

    add ex5

---
+++ ex5.py
@@ -0,0 +1,9 @@
+# Given a String, compute all the characters, except spaces.
+
+# Input : test_str = ‘geeksforgeeks 33 best’ 
+# Output : 19 
+# Explanation : Total characters are 19. 
+
+# Input : test_str = ‘geeksforgeeks best’ 
+# Output : 17 
+# Explanation : Total characters are 17 except spaces.
