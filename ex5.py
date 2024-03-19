2267e694dde59a32cfee83b4cee4b0fe0a20102c
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-19 09:05:23 +0200

    ex5 completed

--- ex5.py
+++ ex5.py
@@ -7,3 +7,9 @@
 # Input : test_str = ‘geeksforgeeks best’ 
 # Output : 17 
 # Explanation : Total characters are 17 except spaces.
+
+def without_space(str):
+  str_without_space = str.replace(" ","")
+  print(len(str_without_space))
+  
+without_space("geeksforgeeks 33 best")
