25743e90e6756955396d8e2acac0f356287e0254
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-20 09:52:56 +0200

    add ex6

---
+++ ex6.py
@@ -0,0 +1,9 @@
+# Given a String, perform uppercase of the later part of the string.
+
+# Input : test_str = 'geeksforgeek' 
+# Output : geeksfORGEEK 
+# Explanation : Latter half of string is uppercased. 
+# Input : test_str = 'apples' 
+# Output : appLES 
+# Explanation : Latter half of string is uppercased.
+
