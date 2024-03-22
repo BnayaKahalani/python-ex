6a52f81d5018e6c8e21d23fad33e0c046f4bb42c
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-22 08:18:49 +0200

    add ex7

---
+++ ex7.py
@@ -0,0 +1,10 @@
+# Python program to capitalize the first and last character of each word in a string
+# Given the string, the task is to capitalize the first and last character of each word in a string. 
+
+# Examples:
+
+# Input: hello world 
+# Output: HellO WorlD
+
+# Input: welcome to geeksforgeeks
+# Output: WelcomE TO GeeksforgeekS
