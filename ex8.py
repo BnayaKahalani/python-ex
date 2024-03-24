92c1f6069c61ded683e22f8837559a67f1ea6d4b
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-24 09:31:25 +0200

    add ex8

---
+++ ex8.py
@@ -0,0 +1,10 @@
+# Given a string in Python. The task is to check whether the string has at least one letter(character) and one number. 
+# Return “True” if the given string fully fill the above condition else return “False” (without quotes).
+
+# Examples: 
+
+# Input: welcome2ourcountry34
+# Output: True
+
+# Input: stringwithoutnum
+# Output: False
