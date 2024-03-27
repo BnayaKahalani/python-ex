901dece76b44899cebe10521cf74ba051beb2bde
Bnaya Kahalani <bnaya50@gmail.com>
2024-03-27 10:02:18 +0200

    add ex9

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Given a string, the task is to check if every vowel is present or not. 
+# We consider a vowel to be present if it is present in upper case or lower case. 
+# i.e. ‘a’, ‘e’, ‘i’.’o’, ‘u’ or ‘A’, ‘E’, ‘I’, ‘O’, ‘U’ . 
+
+# Examples : 
+
+# Input : geeksforgeeks
+# Output : Not Accepted
+# All vowels except 'a','i','u' are not present
+
+# Input : ABeeIghiObhkUul
+# Output : Accepted
+# All vowels are present
+
