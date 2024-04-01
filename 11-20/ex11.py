e821eaefda59cbee56f016ea5bc58dd29fe0d4d1
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-01 09:31:07 +0300

    add ex11

---
+++ 11-20/ex11.py
@@ -0,0 +1,7 @@
+# Given a string, count the number of vowels present in the given string using Sets. 
+# You can use sets to count the number of vowels in a given string in Python. 
+# Sets are useful to efficiently check for counting the unique occurrences of vowels.
+
+# Input : GeeksforGeeks
+# Output : No. of vowels : 5
+# Explaination: The string GeeksforGeeks contains 5 vowels in it 4 letter of 'e' and 1 'o'.
