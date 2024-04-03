a53baec090daeada0b7bdb0d4270f9b9adf21cb2
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-03 10:36:31 +0300

    add ex12

---
+++ 11-20/ex12.py
@@ -0,0 +1,6 @@
+# We are given a string and we need to remove all duplicates from it? 
+# What will be the output if the order of character matters? Examples:
+
+# Input : geeksforgeeks 
+# Output : geksf
+
