15be98ccb8dd0d4add461cabd367c25b699e4024
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-05 11:17:16 +0300

    add ex13

---
+++ 11-20/ex13.py
@@ -0,0 +1,9 @@
+# Given a String list, extract frequency of specific characters in the whole strings list.
+
+# Input : test_list = [“geeksforgeeks is best for geeks”], chr_list = [‘e’, ‘b’, ‘g’, ‘f’] 
+# Output : {‘g’: 3, ‘e’: 7, ‘b’: 1, ‘f’ : 2} 
+# Explanation : Frequency of certain characters extracted. 
+
+# Input : test_list = [“geeksforgeeks”], chr_list = [‘e’, ‘g’] 
+# Output : {‘g’: 2, ‘e’: 4} 
+# Explanation : Frequency of certain characters extracted.
