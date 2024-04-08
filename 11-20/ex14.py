17a5ae0c2d91bc1612e82b4a2f8d9511f09e27f6
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-08 09:26:32 +0300

    add ex14

---
+++ 11-20/ex14.py
@@ -0,0 +1,9 @@
+# Given a string, the task is to check if that string contains any special character (defined special character set). 
+# If any special character is found, don’t accept that string.
+
+# Examples : 
+
+# Input : Geeks$For$Geeks
+# Output : String is not accepted.
+# Input : Geeks For Geeks
+# Output : String is accepted
