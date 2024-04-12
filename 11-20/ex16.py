7d928a31f27578c6c8cd6d6066e1292661b52e14
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-12 09:25:37 +0300

    add ex16

---
+++ 11-20/ex16.py
@@ -0,0 +1,15 @@
+# A string is given, and you have to find all the words (substrings separated by a space) which are greater than the given length k.
+
+# Examples:
+
+# Input : str = "hello geeks for geeks 
+#           is computer science portal" 
+#         k = 4
+# Output : hello geeks geeks computer 
+#          science portal
+# Explanation : The output is list of all 
+# words that are of length more than k.
+# Input : str = "string is fun in python"
+#         k = 3
+# Output : string python 
+
