4e1daefba0b48484d32cb7ccbcb51888f0380a2c
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-16 11:24:02 +0300

    add ex18

---
+++ 11-20/ex18.py
@@ -0,0 +1,9 @@
+# Given string str. The task is to check whether it is a binary string or not. 
+
+# Examples:  
+
+# Input: str = "01010101010"
+# Output: Yes
+
+# Input: str = "geeks101"
+# Output: No
