a5c1614ba9b15180910bcfba197d4778434f4c39
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-18 09:28:00 +0300

    add ex19

---
+++ 11-20/ex19.py
@@ -0,0 +1,7 @@
+# We are given a list of pattern strings and a single input string. 
+# We need to find all possible close good enough matches of input string into list of pattern strings. Examples:
+
+# Input : patterns = ['ape', 'apple', 
+#                   'peach', 'puppy'], 
+#           input = 'appel'
+# Output : ['apple', 'ape']
