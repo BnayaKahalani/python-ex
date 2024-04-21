388dda35ff7b3ba9f043bddf9bb02eeeefe8c016
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-21 17:58:37 +0300

    complete ex20

--- 11-20/ex20.py
+++ 11-20/ex20.py
@@ -9,3 +9,20 @@
 
 # Input : A = “apple banana mango” , B = “banana fruits mango”
 # Output : [‘apple’, ‘fruits’]
+
+def check_uncommon(str1, str2):
+  l1 = str1.split()
+  l2 = str2.split()
+  res = []
+  
+  for word in l1:
+    if word not in l2 and word not in res:
+      res.append(word)
+      
+  for word in l2:
+    if word not in l1 and word not in res:
+      res.append(word)
+      
+  print(res)
+
+check_uncommon("Geeks for Geeks", "Learning from Geeks for Geeks")
