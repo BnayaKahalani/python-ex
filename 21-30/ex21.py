a75269905f17500df89446e90ddf1188c471447b
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-22 12:32:12 +0300

    add ex21

---
+++ 21-30/ex21.py
@@ -0,0 +1,6 @@
+# The problem is quite simple. Given a string, we need to replace all commas with dots and all dots with the commas. 
+# This can be achieved in many different ways. 
+# Examples: 
+
+# Input : 14, 625, 498.002
+# Output : 14.625.498, 002 
