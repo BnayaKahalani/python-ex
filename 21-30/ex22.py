84dc4c51962515727eb8430a7d34e00515abe98a
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-26 10:02:04 +0300

    add ex22

---
+++ 21-30/ex22.py
@@ -0,0 +1,9 @@
+# Given a string S, containing numeric words, the task is to convert the given string to the actual number.
+
+# Example: 
+
+# Input: S = “zero four zero one”
+# Output: 0401
+
+# Input: S = “four zero one four”
+# Output: 4014
