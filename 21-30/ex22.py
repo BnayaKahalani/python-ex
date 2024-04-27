263565fd4f560c103042e21b7d7e89f298a6dc0e
Bnaya Kahalani <bnaya50@gmail.com>
2024-04-27 23:55:56 +0300

    complete ex22

--- 21-30/ex22.py
+++ 21-30/ex22.py
@@ -7,3 +7,14 @@
 
 # Input: S = “four zero one four”
 # Output: 4014
+
+def convert_to_num(s):
+    nums = {
+        "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+        "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
+    }
+    return "".join([nums[word] for word in s.split()])
+
+input_str = "zero four zero one"
+print(convert_to_num(input_str)) 
+
