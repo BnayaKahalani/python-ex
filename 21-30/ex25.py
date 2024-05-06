c2f33d2289d5f76f9853e1d60d5ad968e20a47e7
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-06 23:50:28 +0300

    add ex25

---
+++ 21-30/ex25.py
@@ -0,0 +1,10 @@
+# Given two strings s1 and s2. The task is to find out the minimum number of string rotations for the given string s1 to obtain the actual string s2. 
+# Examples:
+
+# Input : eeksg, geeks
+# Output: 1 
+# Explanation: g is rotated left to obtain geeks.
+
+# Input : eksge, geeks
+# Output: 2
+# Explanation : e and g are left rotated to obtain geeks.
