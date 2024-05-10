f653916d386018dcc195edbb9fb9ab503b2feec8
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-10 08:52:11 +0300

    add ex26

---
+++ 21-30/ex26.py
@@ -0,0 +1,6 @@
+Sometimes while working with Python strings, we can have a problem in which we need to extract the frequency of all the words in a string. 
+
+Input : test_str = 'Gfg is best' 
+Output : {'Gfg': 1, 'is': 1, 'best': 1} 
+Input : test_str = 'Gfg Gfg Gfg' 
+Output : {'Gfg': 3}
