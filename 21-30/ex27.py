55016e4ce4079bea7bc6f77aa6f66f7860544ceb
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-15 11:15:03 +0300

    complete ex27

--- 21-30/ex27.py
+++ 21-30/ex27.py
@@ -4,3 +4,19 @@
 # Output : {'s': 3} 
 # Input : test_str = 'geek', que_word = "geek" 
 # Output : {}
+
+import re 
+     
+test_str = 'geeksforgeeks is best for geeks. A geek should take interest.'
+que_word = "geek"
+
+def freq_next(string, que):  
+  temp = [] 
+  for sub in re.findall(que + '.', string): 
+      temp.append(sub[-1]) 
+  
+  res = {que : temp.count(que) for que in temp} 
+  
+  print("The Characters Frequency is : " + str(res)) 
+  
+freq_next(test_str, que_word)
