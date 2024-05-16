d640e6adb1b31720e17db3f190239b66715942d1
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-16 18:42:32 +0300

    add ex28

---
+++ 21-30/ex28.py
@@ -0,0 +1,9 @@
+# Given String list, perform sort operation on basis of frequency of particular character.
+
+# Input : test_list = [“geekforgeekss”, “is”, “bessst”, “for”, “geeks”], K = ‘s’ 
+# Output : [‘bessst’, ‘geekforgeekss’, ‘geeks’, ‘is’, ‘for’] 
+# Explanation : bessst has 3 occurrence, geeksforgeekss has 3, and so on.
+
+# Input : test_list = [“geekforgeekss”, “is”, “bessst”], K = ‘e’ 
+# Output : [“geekforgeekss”, “bessst”, “is”] 
+# Explanation : Ordered decreasing order of ‘e’ count. 
