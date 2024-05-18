e764eea191cb3e95911ad8ac5f5e87066eed0df8
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-18 23:48:15 +0300

    complete ex28

--- 21-30/ex28.py
+++ 21-30/ex28.py
@@ -7,3 +7,12 @@
 # Input : test_list = [“geekforgeekss”, “is”, “bessst”], K = ‘e’ 
 # Output : [“geekforgeekss”, “bessst”, “is”] 
 # Explanation : Ordered decreasing order of ‘e’ count. 
+
+def sort_freq(test_list):
+ 
+  print("The original list is : " + str(test_list))
+  K = 'e'
+  res = sorted(test_list, key = lambda ele: -ele.count(K))
+  print("Sorted String : " + str(res))
+  
+sort_freq(["geekforgeeks", "is", "best", "for", "geeks"])
