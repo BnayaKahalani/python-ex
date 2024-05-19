d116839c7473fd26d41269539a52c95fb64b6061
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-19 09:45:51 +0300

    add ex29

---
+++ 21-30/ex29.py
@@ -0,0 +1,3 @@
+# Sometimes, while working with Python Strings, we have problem in which we need to perform a case conversion of String. 
+
+# Input : geeks_for_geeks Output : GeeksforGeeks Input : left_index Output : leftIndex
