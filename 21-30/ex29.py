eafad5e4c12d79db203621d10997b84a7edbb60d
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-20 09:28:01 +0300

    complete ex29

--- 21-30/ex29.py
+++ 21-30/ex29.py
@@ -1,3 +1,13 @@
 # Sometimes, while working with Python Strings, we have problem in which we need to perform a case conversion of String. 
 
 # Input : geeks_for_geeks Output : GeeksforGeeks Input : left_index Output : leftIndex
+
+import string
+
+def case_conv(test_str):
+  
+  print("The original string is : " + test_str)
+  res = string.capwords(test_str.replace("_", " ")).replace(" ", "")
+  print("The String after changing case : " + str(res)) 
+
+case_conv('geeksforgeeks_is_best')
