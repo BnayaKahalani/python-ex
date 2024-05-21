ea1157df0bd9b01ff0dc88b4b1206e162b955c5c
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-21 09:58:00 +0300

    add ex30

---
+++ 21-30/ex30.py
@@ -0,0 +1,10 @@
+# Given an Integer list, perform join with the delimiter, avoiding the extra delimiter at the end.
+
+# Input : test_list = [4, 7, 8, 3, 2, 1, 9], delim = “*” 
+# Output : 4*7*8*3*2*1*9 
+# Explanation : The rear “*” which usually occurs in concatenation, is avoided.
+
+# Input : test_list = [4, 7, 8, 3], delim = “*” 
+# Output : 4*7*8*3 
+# Explanation : The rear “*” which usually occurs in concatenation, is avoided. 
+
