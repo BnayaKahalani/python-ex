a46bbee54b18348e47acc24251f84a2b672d1eb8
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-22 19:00:37 +0300

    complete ex30

--- 21-30/ex30.py
+++ 21-30/ex30.py
@@ -8,3 +8,14 @@
 # Output : 4*7*8*3 
 # Explanation : The rear “*” which usually occurs in concatenation, is avoided. 
 
+def join_delimiter(test_list):
+ 
+  print("The original list is : " + str(test_list))
+  
+  delim = "$"
+  
+  res = delim.join(map(str, test_list))
+  
+  print("The joined string : " + str(res))
+
+join_delimiter([4, 7, 8, 3, 2, 1, 9])
