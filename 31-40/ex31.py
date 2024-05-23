30eb21e53b9e29a0808f3276e7e286a03fa65b04
Bnaya Kahalani <bnaya50@gmail.com>
2024-05-23 12:43:38 +0300

    add ex31

---
+++ 31-40/ex31.py
@@ -0,0 +1,12 @@
+# Given a list of strings. 
+# The task is to find the index of the character position for the word, which lies at the Kth index in the list of strings.
+
+# Examples:
+
+# Input : test_list = [“geekforgeeks”, “is”, “best”, “for”, “geeks”], K = 21 
+# Output : 0
+# Explanation : 21st index occurs in “geeks” and point to “g” which is 0th element of word.
+
+# Input : test_list = [“geekforgeeks”, “is”, “best”, “for”, “geeks”], K = 15 
+# Output : 1
+# Explanation : 15th index occurs in “best” and point to “e” which is 1st element of word.
